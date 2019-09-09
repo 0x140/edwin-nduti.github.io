8631626d71e107fa43ac3cfcdeec84341607b876
Edwin Nduti <edwin@localhost.localdomain>
2019-09-09 10:16:23 -0400

    Initial commit

---
+++ event.js
@@ -0,0 +1,8 @@
+function hoverColor() {
+
+        document.getElementById("heading1").setAttribute("style","color:red");     
+
+}
+
+document.getElementById("heading1").addEventListener('mouseover',hoverColor);
+
